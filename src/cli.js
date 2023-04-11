6fd8a9d66247362d73d2f39a672ff4caa526b042
Vitaly Portnov <vitport@hotmail.com>
2023-04-11 15:32:23 +0300

    fixed teacher comment #6

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const NameQuestion = () => {
+const makeWelcome = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
-export default NameQuestion;
+export default makeWelcome;
