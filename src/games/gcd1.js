e554d6f6467a7bd33a52709863aca7a2ef0f335e
Vitaly Portnov <vitport@hotmail.com>
2023-04-11 15:30:50 +0300

    GCD game changed for engine

---
+++ src/games/gcd1.js
@@ -0,0 +1,31 @@
+import runEngine from '../index.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const getRandomNumber = () => Math.ceil(Math.random() * 100);
+
+const generateRound = () => {
+  const numbers = [getRandomNumber(), getRandomNumber()];
+  const firstNumber = Math.max(numbers[0], numbers[1]);
+  const secondNumber = Math.min(numbers[0], numbers[1]);
+  const question = `${firstNumber} ${secondNumber}`;
+  let marker = 0;
+  let devider = secondNumber;
+  let answer = 0;
+  while (marker === 0) {
+    if (firstNumber % devider === 0 && secondNumber % devider === 0) {
+      answer = devider;
+      marker = 1;
+      break;
+    } else {
+      devider -= 1;
+    }
+  }
+  return [question, String(answer)];
+};
+
+const runGcd = () => runEngine(rules, generateRound);
+
+export default runGcd;
+
+runGcd();
